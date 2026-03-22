01885f7763568ecb0302c0c347b09915dff46374
daosolido <daosolido@gmail.com>
2026-03-23 00:10:23 +0800

    Update planet.js

--- api/planet.js
+++ api/planet.js
@@ -1,21 +1,18 @@
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
   // CORS для Google Sheets
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
-  // Обработка предварительного запроса OPTIONS
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
   
-  // Только POST
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed. Use POST.' });
   }
   
   try {
-    // Получаем параметры
     const { year, month, day, hour, minute, second, planetId } = req.body;
     
     // Проверка параметров
@@ -31,15 +28,43 @@ module.exports = async (req, res) => {
     }
     dayOfYear += day;
     
+    // Учет високосного года
+    const isLeap = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+    if (isLeap && month > 2) dayOfYear += 1;
+    
     // Часы в десятичном формате
     const hourDecimal = (hour || 12) + (minute || 0) / 60 + (second || 0) / 3600;
     
     // Количество дней с 2000 года
     const daysSince2000 = (year - 2000) * 365.25 + dayOfYear + hourDecimal / 24;
     
-    // Точные средние долготы на 2000.0 (VSOP87)
-    const L0 = [280.46646, 218.316, 252.250, 181.979, 355.433, 34.351, 50.077, 313.232, 304.348, 238.928];
-    const n = [0.9856474, 13.176358, 4.092335, 1.602130, 0.524038, 0.083090, 0.033457, 0.011723, 0.005957, 0.003955];
+    // Точные средние долготы на 1 января 2000 года (VSOP87)
+    const L0 = [
+      280.46646,  // 0: Солнце
+      218.316,    // 1: Луна
+      252.250,    // 2: Меркурий
+      181.979,    // 3: Венера
+      355.433,    // 4: Марс
+      34.351,     // 5: Юпитер
+      50.077,     // 6: Сатурн
+      313.232,    // 7: Уран
+      304.348,    // 8: Нептун
+      238.928     // 9: Плутон
+    ];
+    
+    // Средние движения (градусов в день)
+    const n = [
+      0.9856474,  // 0: Солнце
+      13.176358,  // 1: Луна
+      4.092335,   // 2: Меркурий
+      1.602130,   // 3: Венера
+      0.524038,   // 4: Марс
+      0.083090,   // 5: Юпитер
+      0.033457,   // 6: Сатурн
+      0.011723,   // 7: Уран
+      0.005957,   // 8: Нептун
+      0.003955    // 9: Плутон
+    ];
     
     // Расчет долготы
     let longitude = L0[planetId] + n[planetId] * daysSince2000;
@@ -50,7 +75,7 @@ module.exports = async (req, res) => {
       value: longitude,
       planetId: planetId,
       date: `${year}-${month}-${day}`,
-      time: hourDecimal
+      julianDays: daysSince2000
     });
     
   } catch (error) {
