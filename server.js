88fbe1945dbff1cfd650deac41759474d0c7dbd7
daosolido <daosolido@gmail.com>
2026-03-23 14:04:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const { load, Constants } = require('@fusionstrings/swiss-eph');
+import express from 'express';
+import { Constants, load } from '@fusionstrings/swiss-eph';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
